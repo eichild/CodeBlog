c774f0581f13d6ba26d2ce212f0b7fd892f7d510
Lucas Gregorio Martins <lucas.martins@tqi.com.br>
2020-11-10 16:22:16 -0300

    Adicionado Cards para visualização dos posts

--- codeblog/src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ codeblog/src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -4,6 +4,7 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +22,14 @@ public class CodeblogController {
     public ModelAndView getPosts(){
          ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = codeblogService.FindAll();
+        mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostsDetails(@PathVariable("id") Long id){
+        ModelAndView mv = new ModelAndView("posts");
+        Post posts = codeblogService.FindById(id);
         mv.addObject("posts", posts);
         return mv;
     }
